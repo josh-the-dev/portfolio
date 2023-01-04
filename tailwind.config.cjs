2b9355e933a9338b823c0e77095285cf11341f94
Joshua Grant <grantjoshua1995@gmail.com>
2023-01-04 22:56:57 +0100

    adding basic portfolio layout and theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'josh': {
+					'blue': {
+						100: '#31446B',
+						200: '#14142D',
+					},
+					'sage': '#4D8A95',
+					'beige': '#D0CDB3',
+					'orange': '#DFB64C'
+				}
+			},
+			fontFamily: {
+				'open-sans': ['"Open Sans"']
+			}
+		},
 	},
 	plugins: [],
 }
