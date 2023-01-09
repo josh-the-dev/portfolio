7da559a64cf6c0994352f5a720bb6d775bd638fa
Joshua Grant <grantjoshua1995@gmail.com>
2023-01-09 20:48:50 +0100

    add project component and prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
@@ -10,5 +10,5 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,23 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				'josh': {
-					'blue': {
-						100: '#31446B',
-						200: '#14142D',
-					},
-					'sage': '#4D8A95',
-					'beige': '#D0CDB3',
-					'orange': '#DFB64C'
-				}
-			},
-			fontFamily: {
-				'open-sans': ['"Open Sans"']
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        josh: {
+          blue: {
+            100: "#31446B",
+            200: "#14142D",
+          },
+          sage: "#4D8A95",
+          beige: "#D0CDB3",
+          orange: "#DFB64C",
+        },
+      },
+      fontFamily: {
+        "open-sans": ['"Open Sans"'],
+      },
+    },
+  },
+  plugins: [],
+};
