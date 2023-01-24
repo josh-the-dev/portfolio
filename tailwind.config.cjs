e9a1c3f116bd0453380c0a30855c082fe7201815
Joshua Grant <grantjoshua1995@gmail.com>
2023-01-24 20:36:56 +0100

    remove bulk of content and re-theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,9 +6,6 @@ module.exports = {
       colors: {
         'dark-blue': '#31446B',
         'floral-white': '#FFF8F0',
-        'dark-red': '#92140C',
-        'deep-champagne': '#FFCF99',
-        'emerald': '#0CCE6B'
       },
       fontFamily: {
         "open-sans": ['"Open Sans"'],
